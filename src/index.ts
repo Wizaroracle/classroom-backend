357275ae9d57eecb3340b6651b4cde23ca6571f7
Christian Jonel Cañanes <christianjonelcananes@gmail.com>
2026-03-05 14:52:41 +0800

    implement GET ALL subjects route

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 import express from "express";
-
+import subjectsRouter from "./routes/subjects";
+import cors from "cors";
 const app = express();
 const PORT = 8000;
 
+//Cors
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL, // React app URL
+    methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
+    credentials: true, // allow cookies
+  }),
+);
 // Middleware
 app.use(express.json());
+//Routes
+app.use("/api/subjects", subjectsRouter);
 
 // Root GET route
 app.get("/", (req, res) => {
