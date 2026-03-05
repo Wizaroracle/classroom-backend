6156c88e81430098f24d793a1676a89fa86be6db
Christian Jonel Cañanes <christianjonelcananes@gmail.com>
2026-03-05 15:02:21 +0800

    revert the database variable

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
-if (!process.env.VITE_DATABASE_URL) {
+if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not set in .env file");
 }
 
@@ -10,6 +10,6 @@ export default defineConfig({
   out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.VITE_DATABASE_URL,
+    url: process.env.DATABASE_URL,
   },
 });
